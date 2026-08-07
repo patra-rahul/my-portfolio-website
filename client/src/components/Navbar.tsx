d89aa01718251da24433307b2d21f5b70ca34ec8
patra-rahul <contactrahulpatra@gmail.com>
2026-08-07 20:09:55 +0530

    write responsive properties for navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import logo from "../assets/owl.jpg";
+import { SquareMenu } from "lucide-react";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="p-3 m-4 bg-panel bg-opacity-0.5 rounded-xl">
-      <ul className="flex justify-between items-center text-4xl text-color-panel text-primary-text">
+      <ul className="hidden md:flex justify-between items-center text-4xl text-color-panel text-primary-text">
         <li>
-          <img src={logo} alt="inaccessible" className="h-12 rounded-full object-contain" />
+          <img
+            src={logo}
+            alt="inaccessible"
+            className="h-12 rounded-full object-contain"
+          />
         </li>
         <li>Work</li>
         <li>About</li>
@@ -17,6 +25,38 @@ const Navbar = () => {
           </a>
         </li>
       </ul>
+
+      <ul className="md:hidden flex items-center justify-between">
+        <li>
+          <img
+            src={logo}
+            alt="inaccessible"
+            className="h-12 rounded-full object-contain"
+          />
+        </li>
+        <li>
+          <button
+            className="px-4 py-2 rounded-lg text-white"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <SquareMenu className="h-10 w-10" />
+          </button>
+        </li>
+      </ul>
+
+      {menuOpen && (
+        <ul className="flex-col justify-center space-y-2 text-white mt-4 tracking-widest ">
+          <li className="bg-[#262f3b] p-2 rounded-lg">Work</li>
+          <li className="bg-[#262f3b] p-2 rounded-lg">About</li>
+          <li className="bg-[#262f3b] p-2 rounded-lg">Github</li>
+          <li className="bg-[#262f3b] p-2 rounded-lg">Activity</li>
+          <li className="bg-white px-4 py-2 rounded-lg text-bg">
+            <a href="mailto:contractrahulpatra@gmail.com">
+              contactrahulpatra@gmail.com
+            </a>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
