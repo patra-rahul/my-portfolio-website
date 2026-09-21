2555840d611156bd956436747db7de22513faf81
patra-rahul <contactrahulpatra@gmail.com>
2026-09-21 23:37:04 +0530

    update(About): build the self-photo, about-me, tech-stack & project-count cards

--- client/src/components/AboutV2.tsx
+++ client/src/components/AboutV2.tsx
@@ -1,12 +1,28 @@
 import OwlV2 from "../assets/images/owlv2.jpg";
 import { ChevronRight } from "lucide-react";
 
+import Cpp from "../assets/icons/CPP.svg";
+import Js from "../assets/icons/JavaScript.svg";
+import Py from "../assets/icons/Python.svg";
+import Ts from "../assets/icons/TypeScript.svg";
+import React from "../assets/icons/React.svg";
+import Tailwind from "../assets/icons/Tailwind CSS.svg";
+import Express from "../assets/icons/Express.svg";
+import Mongoose from "../assets/icons/Mongoose.svg";
+import Prisma from "../assets/icons/prisma.svg";
+import Git from "../assets/icons/Git.svg";
+import Github from "../assets/icons/GitHub.svg";
+import Mongo from "../assets/icons/MongoDB.svg";
+import Postgres from "../assets/icons/PostgresSQL.svg";
+import Redis from "../assets/icons/Redis.svg";
+import Postman from "../assets/icons/Postman.svg";
+import Node from "../assets/icons/Node.svg";
+
 const AboutV2 = () => {
   return (
     <>
       <div className="p-2 m-2 text-white">
         <div className="grid grid-cols-1 md:grid-cols-12 md:auto-rows-[90px] gap-4">
-
           <div className="col-span-8 row-span-4 rounded-2xl bg-bg overflow-hidden border border-white/10 p-5 flex flex-col justify-center px-20">
             <h1 className="text-2xl ">Rahul Patra [Synonyms: Owl] </h1>
             <p>
@@ -47,19 +63,128 @@ const AboutV2 = () => {
             <img src={OwlV2} alt="" />
           </div>
 
-          <div className="col-span-4 row-span-6 border border-white/10 rounded-2xl overflow-hidden">
+          <div className="col-span-4 row-span-7 border border-white/10 rounded-2xl overflow-hidden">
             Card 3
           </div>
 
-          <div className="col-span-8 row-span-2 border border-white/10 rounded-2xl overflow-hidden">
-            Card 4
+          <div className="col-span-8 row-span-3 border border-white/10 rounded-2xl overflow-hidden bg-white/6">
+            <div className="grid grid-cols-12 p-4 items-center gap-y-2 mt-4.5 ml-5">
+              <h1 className="col-span-8 text-3xl">Languages</h1>
+              <img
+                src={Cpp}
+                alt="C++"
+                className="col-span-1"
+                height={40}
+                width={40}
+              />
+              <img
+                src={Js}
+                alt="C++"
+                className="col-span-1"
+                height={40}
+                width={40}
+              />
+              <img
+                src={Ts}
+                alt="C++"
+                className="col-span-1"
+                height={40}
+                width={40}
+              />
+              <img
+                src={Py}
+                alt="C++"
+                className="col-span-1"
+                height={40}
+                width={40}
+              />
+
+              <h1 className="col-span-9 text-3xl">Frameworks</h1>
+              <img
+                src={React}
+                alt=""
+                className="col-span-1"
+                height={40}
+                width={40}
+              />
+              <img
+                src={Tailwind}
+                alt=""
+                className="col-span-1"
+                height={40}
+                width={40}
+              />
+              <img
+                src={Express}
+                alt=""
+                className="col-span-1 invert"
+                height={40}
+                width={40}
+              />
+
+              <h1 className="col-span-9 text-3xl">Database & Cache</h1>
+              <img
+                src={Mongo}
+                alt=""
+                className="col-span-1"
+                height={40}
+                width={40}
+              />
+              <img
+                src={Postgres}
+                alt=""
+                className="col-span-1"
+                height={40}
+                width={40}
+              />
+              <img
+                src={Redis}
+                alt=""
+                className="col-span-1"
+                height={40}
+                width={40}
+              />
+
+              <h1 className="col-span-10 text-3xl">ORMs & ODMs</h1>
+              <img
+                src={Prisma}
+                alt=""
+                className="col-span-1 brightness-0 invert"
+                height={40}
+                width={40}
+              />
+              <img
+                src={Mongoose}
+                alt=""
+                className="col-span-1"
+                height={40}
+                width={40}
+              />
+
+              <h1 className="col-span-10 text-3xl">Version Control Tools</h1>
+              <img
+                src={Git}
+                alt=""
+                className="col-span-1"
+                height={40}
+                width={40}
+              />
+              <img
+                src={Github}
+                alt=""
+                className="col-span-1 invert brightness-0"
+                height={40}
+                width={40}
+              />
+            </div>
           </div>
 
           <div className="col-span-5 row-span-4 border border-white/10 rounded-2xl overflow-hidden">
             Card 5
           </div>
-          <div className="col-span-3 row-span-2 border border-white/10 rounded-2xl overflow-hidden">
-            Card 6
+          <div className="col-span-3 row-span-2 bg-purple rounded-2xl overflow-hidden flex flex-col items-center justify-center text-center">
+            <span className="text-6xl">7+</span>
+            Fundamental Backend Projects
           </div>
           <div className="col-span-3 row-span-2 border border-white/10 rounded-2xl overflow-hidden">
             Card 7
